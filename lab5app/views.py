5cfa967e9bbca180d1e3fa2e8f6c4d12673eddef
osamaAlharbi1 <sdfd567uu@gmail.com>
2024-03-14 21:30:45 +0300

    update file

--- lab5app/views.py
+++ lab5app/views.py
@@ -1,31 +1,43 @@
+from django import forms
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
-from django.http import HttpResponseRedirect , HttpResponse
 from django.urls import reverse
 
+people = []
 
 class Person:
-    def __init__(self, username, password):
-        self.username = username
+    def __init__(self, name, password):
+        self.username = name
         self.password = password
 
     def __str__(self):
         return self.username
 
-people = []
+class NewPersonForm(forms.Form):
+    name = forms.CharField(label="Username")
+    password = forms.CharField(label="Password", widget=forms.PasswordInput)    
+
+# Create your views here.
+def default(request):      
+    return render(request, "lab5app/default.html", {
+        "people": people
+    })
 
 def add(request):
-    if request.method == 'POST':
-        username = request.POST.get('username')
-        password = request.POST.get('password')
-        person = Person(username, password)
-        people.append(person)
-        return HttpResponseRedirect(reverse('default'))
-    return render(request, 'lab5app/add.html')
+    if request.method == "POST":
+        form = NewPersonForm(request.POST)
 
-def default(request):
-    return render(request, 'lab5app/default.html', {'people': people})
-
-
-
-
-
+        if form.is_valid():
+            name = form.cleaned_data["name"]
+            password = form.cleaned_data["password"]
+            person = Person(name,password)
+            people.append(person)
+            return HttpResponseRedirect(reverse("default"))
+        else:
+            return render(request, "lab5app/add.html",{
+                "form": form
+            })
+    else:
+        return render(request, "lab5app/add.html",{
+            "form": NewPersonForm()
+        })
